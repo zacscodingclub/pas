50778d5b391b329bff31fd9c45ad476dde96f372
Zac Baston <zbaston@tableau.com>
2019-01-07 08:10:47 -0800

    format result for slack

--- result.go
+++ result.go
@@ -25,13 +25,12 @@ func (r *result) toString() string {
 	return sb.String()
 }
 
+// price, title, time left (bids)
 func (r *result) toSlack() string {
 	var sb strings.Builder
-	sb.WriteString(fmt.Sprintf("{ id: %s,", r.id))
-	sb.WriteString(fmt.Sprintf(" title: %s,", r.title))
-	sb.WriteString(fmt.Sprintf(" state: %s,", r.state))
-	sb.WriteString(fmt.Sprintf(" timeLeft: %s,", r.timeLeft))
-	sb.WriteString(fmt.Sprintf(" bids: %d,", r.bids))
-	sb.WriteString(fmt.Sprintf(" currentPrice: %d }", r.currentPrice))
+	sb.WriteString(fmt.Sprintf("$%d,", r.currentPrice))
+	sb.WriteString(fmt.Sprintf(" <%s%s%s|%s>,", c.BaseURL, c.ItemPath, r.id, r.title))
+	sb.WriteString(fmt.Sprintf(" %s", r.timeLeft))
+	sb.WriteString(fmt.Sprintf(" (%d bids)\n", r.bids))
 	return sb.String()
 }
